f01d2436171c7dd79b6d35017cb9c69434f3fb6e
coldsnape <coldsnape@msn.com>
2015-10-29 05:04:18 +0000

    commit

--- public/javascripts/chart-app.js
+++ public/javascripts/chart-app.js
@@ -5,11 +5,20 @@ app.controller('MainController', ['$scope', '$http',  function($scope, $http) {
   $http.get('/data').success(function(data){
     
   var dataArray = formatDataForView(data);
+  
+  dataArray.splice(1,1);
 
   var table = google.visualization.arrayToDataTable(dataArray, false);
   var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
   
-  var options = {'title':'Company Sales'}
+   var options = {
+      title: 'Nitrogen Consumed by Crops from 1965 - 2010',
+      hAxis: {title: 'Year'},
+      vAxis: {title: 'Nitrogen in Tons'},
+      //curveType: 'function',
+      legend: { position: 'bottom' },
+      //colors: ['#1b9e77', '#d95f02', '#7570b3', '#db9500']
+      };
   chart.draw(table, options);
 
   });
@@ -31,7 +40,8 @@ function formatDataForView(data) {
         var dataEntry = [];
         for(var prop in value) {
           dataEntry.push(parseInt(value[prop], 0));
-        }
+          }
+        
         dataArray.push(dataEntry);
     });
   
